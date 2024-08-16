5f471b42de2dd7dde163b2f69b32b69ec500a19a
wolf2lyon <josesebastiancu@gmail.com>
2024-08-16 12:54:27 -0500

    finish testimonials grid section

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
--- style-guide.md
+++ style-guide.md
@@ -14,6 +14,7 @@ The designs were created to the following widths:
 ### Primary
 
 Moderate violet: hsl(263, 55%, 52%)
+
 Very dark grayish blue: hsl(217, 19%, 35%)
 Very dark blackish blue: hsl(219, 29%, 14%)
 White: hsl(0, 0%, 100%)
